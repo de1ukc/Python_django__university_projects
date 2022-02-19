2c39f4e6985fd82007ace073e696306d401a83ca
Aleh <igrock.iks@mail.ru>
2022-02-20 01:33:53 +0300

    Все слова в тексте почти готовы - остались скобки черещ регулярки

--- solution.py
+++ solution.py
@@ -7,40 +7,59 @@ def repeatOfEveryWord(fileName):
 
     file = open(fileName,'r')
     text = file.read()
-    text.replace('т.к.','так как',text.count('т.к.'))
-    text.replace('т.п.','тому подобное',text.count('т.п.'))
+    text = text.lower()  # всё к нижнему регистру
+    text = text.lstrip()  # удаляю пробелы в начале
+    text = text.rstrip()  # удаляю пробелы в конце
 
-    text.lower() #всё к нижнему регистру
-    text.lstrip() # удаляю пробелы в начале
-    text.rstrip() # удаляю пробелы в конце
+    text = text.replace('т.к.','так как',text.count('т.к.'))
+    text = text.replace('т.п.','тому подобное',text.count('т.п.'))
+    text = text.replace('т.д.', 'так далее', text.count('т.д.'))
+    text = text.replace('...', '', text.count('...'))
+    text = text.replace('-','',text.count('-'))
+
     listOfWords = text.split()
-
+    outputList = listOfWords.copy()
+    print(listOfWords)
 
     for word in listOfWords:
-        symb = word[len(word - 1)]
-        if word[:3] == '...':
-            word.replace('...','')
+        symb = word[-1]
         for sign in stopSymbols:
-            if (symb == sign) and (word not 'т.д.') and (word not 'т.п.'):
+            if sign == symb:
+                outputList.remove(word)
+                word = word.replace(symb,'')
+                outputList.append(word)
+        if word[0] == '"' and word[-1] == '"':
+            outputList.remove(word)
+            word = word.replace('"', '', 2)
+            outputList.append(word)
+        if word[0] == '"' or word[-1] == '"':
+            outputList.remove(word)
+            word = word.replace('"','',1)
+            outputList.append(word)
+
+        #переписать говно выше через регулярные выражения
 
 
-
-
-
-    print(text)
-
-
-
+    for word in outputList:
+        if word in ans:
+            ans[word] += 1
+        else:
+            ans[word] = 1
 
     return ans
 
-def test():
-    text = "abcd."
+def repeatOfEveryWordtoFile(fileName,dic):
+    file = open(fileName,"w")
+    words = dic
 
-    print(text[len(text)-1])
+    for word in words:
+        file.write(word + ":" + str(words[word]) + "\n")
+    file.write("\n")
 
+    file.close()
+    return
 
 
 if __name__ == "__main__":
-    #repeatOfEveryWord("text.txt")
-    test()
+   repeatOfEveryWordtoFile("output.txt",repeatOfEveryWord("text.txt"))
+   #test()
