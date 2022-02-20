9dfd6379ddbddfd010b7c0f67f087a5e505475e7
Aleh <igrock.iks@mail.ru>
2022-02-20 12:50:56 +0300

    Добавлена функция расчёта среднего и медианного количества слов в предложении

--- solution.py
+++ solution.py
@@ -1,8 +1,8 @@
-#функция будет возвращать словарь с количеством повторений каждого слова
-
+import math
+from statistics import median
 stopSymbols = ('.',',','?','!',':',';')
 
-
+#функция будет возвращать словарь с количеством повторений каждого слова
 def repeatOfEveryWord(fileName):
     ans = dict()
 
@@ -16,6 +16,7 @@ def repeatOfEveryWord(fileName):
     text = text.replace('т.к.','так как',text.count('т.к.'))
     text = text.replace('т.п.','тому подобное',text.count('т.п.'))
     text = text.replace('т.д.', 'так далее', text.count('т.д.'))
+    text = text.replace('др.','другое',text.count('др.'))
     text = text.replace('...', '', text.count('...'))
     text = text.replace('-','',text.count('-'))
     text = text.replace('(','',text.count('('))
@@ -40,10 +41,10 @@ def repeatOfEveryWord(fileName):
             ans[word] += 1
         else:
             ans[word] = 1
-
     return ans
 
-def repeatOfEveryWordtoFile(fileName,dic):
+#функция записывать полученный словарь ответов в файл
+def repeatOfEveryWordToFile(fileName,dic):
     file = open(fileName,"w")
     words = dic
 
@@ -54,7 +55,86 @@ def repeatOfEveryWordtoFile(fileName,dic):
     file.close()
     return
 
+def averageNumberOfWordsInASentence(fileName):
+    countOfWords = 0
+    countOfSentences = 0
+
+    text = textHelper(fileName)
+
+    #получения количества слов.
+    buff = text.split()
+    countOfWords = len(buff)
+    countOfSentences = len(text.split('.')) - 1
+    print(countOfWords)
+    print(countOfSentences)
+
+    if countOfSentences == 0:
+        return 0
+    else:
+        return countOfWords/countOfSentences
+
+def averageNumberOfWordsInASentenceToFile(fileName,ans):
+    file = open(fileName, "a")
+
+    file.write("Среднее количество слов в предложении = " + str(ans))
+    file.write('\n')
+
+    file.close()
+    return
+
+def textHelper(fileName):
+    file = open(fileName, 'r')
+
+    text = file.read()
+    text = text.lstrip()  # удаляю пробелы в начале
+    text = text.rstrip()  # удаляю пробелы в конце
+    text = text.replace('?', '.').replace('!', '.').replace('...', '.')
+    text = text.replace('-', '', text.count('-'))
+
+    text = text.replace('т.к.', 'так как', text.count('т.к.'))
+    text = text.replace('т.п.', 'тому подобное', text.count('т.п.'))
+    text = text.replace('т.д.', 'так далее', text.count('т.д.'))
+    text = text.replace('др.', 'другое', text.count('др.'))
+    file.close()
+    return text
+
+def medianNumberOfWordsInSentence(fileName):
+    text = textHelper(fileName)
+
+    listOfNumbersForEverySentence = []
+
+    listOfSentences = text.split(".")
+    listOfSentences.remove('')
+
+    for sentense in listOfSentences:
+        words = sentense.split()
+        listOfNumbersForEverySentence.append(len(words))
+    listOfNumbersForEverySentence.sort()
+
+    medianAns = 0.
+    medianPos = (len(listOfNumbersForEverySentence) + 1)/2
+    if medianPos is not int:
+        medianAns = (listOfNumbersForEverySentence[math.ceil(medianPos) - 1] + listOfNumbersForEverySentence[math.floor(medianPos) - 1]) /2
+    else:
+        medianAns = listOfNumbersForEverySentence[int(medianPos) - 1]
+
+    #print(median(listOfNumbersForEverySentence))
+
+    return medianAns
+
+def medianNumberOfWordsInSentenceToFile(fileName,ans):
+    file = open(fileName,"a")
+
+    file.write("Медианное количество слов в предложении = " + str(ans))
+    file.write('\n')
+
+    file.close()
+    return
+
+def main():
+    repeatOfEveryWordToFile("output.txt", repeatOfEveryWord("text.txt")) # количество всех слов в тексте
+    averageNumberOfWordsInASentenceToFile("output.txt",averageNumberOfWordsInASentence("text.txt"))
+    medianNumberOfWordsInSentenceToFile("output.txt",medianNumberOfWordsInSentence("text.txt"))
 
 if __name__ == "__main__":
-   repeatOfEveryWordtoFile("output.txt",repeatOfEveryWord("text.txt"))
-   #test()
+   main()
