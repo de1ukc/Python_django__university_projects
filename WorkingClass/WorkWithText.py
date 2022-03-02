7385883a58513ce579d54f00ad90e51b1845fbaf
Aleh <igrock.iks@mail.ru>
2022-03-02 13:40:01 +0300

    Добавил снэк кейс, немного подправил код

--- WorkingClass/WorkWithText.py
+++ WorkingClass/WorkWithText.py
@@ -1,65 +1,57 @@
 import math
+from WorkingClass.constants import stop_symbols
+from WorkingClass.constants import stop_symbols2
 
 class WorkWithText:
     """Класс для работы с входным текстом"""
 
     def __init__(self) -> None:
-        '''Конструктор класса'''
-        self.stopSymbols = ['.', ',', '?', '!', ':', ';']
-        self.stopSymbols2 = [',', '?', '!', ':', ';']
-
         return
 
-    def RepeatOfEveryWord(self,fileName) -> dict:
+    def repeat_of_every_word(self, file_name) -> dict:
         """
-                    Метод нахождения количества повторений каждого слова в тексте.
-                    :rtype: dict
-                    :return: Возвращает контейнер с повторяющимися словами и количеством их повторений.
-                """
-        ans = dict()
+        Метод нахождения количества повторений каждого слова в тексте.
+        :rtype: dict
+        :return: Возвращает контейнер с повторяющимися словами и количеством их повторений.
+        """
 
-        file = open(fileName, 'r')
-        text = file.read()
-        text = text.lower()  # всё к нижнему регистру
-        text = text.lstrip()  # удаляю пробелы в начале
-        text = text.rstrip()  # удаляю пробелы в конце
+        response = dict()
 
-        # удаляем всё парные знаки, тире. Расшифровываю сокращения в полные слова
-        text = text.replace('т.к.', 'так как', text.count('т.к.'))
-        text = text.replace('т.п.', 'тому подобное', text.count('т.п.'))
-        text = text.replace('т.д.', 'так далее', text.count('т.д.'))
-        text = text.replace('др.', 'другое', text.count('др.'))
-        text = text.replace('...', '', text.count('...'))
+        text = self.text_helper(file_name)
+
         text = text.replace('-', '', text.count('-'))
         text = text.replace('(', '', text.count('('))
         text = text.replace(')', '', text.count(')'))
         text = text.replace('"', '', text.count('"'))
         text = text.replace("'", '', text.count("'"))
-        text.replace("/", '', text.count('/'))
+        text = text.replace("/", '', text.count('/'))
 
-        listOfWords = text.split()
-        outputList = listOfWords.copy()
+        list_of_words = text.split()
+        output_list = list_of_words.copy()
 
-        for word in listOfWords:
+        for word in list_of_words:
             symb = word[-1]
-            for sign in self.stopSymbols:
+            for sign in stop_symbols:
                 if sign == symb:
-                    outputList.remove(word)
+                    output_list.remove(word)
                     word = word.replace(symb, '')
-                    outputList.append(word)
+                    output_list.append(word)
 
-        for word in outputList:
-            if word in ans:
-                ans[word] += 1
+        for word in output_list:
+            if word in response:
+                response[word] += 1
             else:
-                ans[word] = 1
-        return ans
+                response[word] = 1
 
-    def RepeatOfEveryWordToFile(self,fileName,dic) -> None:
-        '''Функция записывает результат работы аналогичного метода в файл
+        return response
 
-        '''
-        file = open(fileName,"w")
+    def repeat_of_every_word_to_file(self, file_name, dic) -> None:
+        """
+        Функция записывает результат работы аналогичного метода в файл
+        """
+
+        file = open(file_name, "w")
+
         words = dic
 
         for word in words:
@@ -67,154 +59,172 @@ class WorkWithText:
         file.write("\n")
 
         file.close()
+
         return
 
-    def RepeatOfEveryWordToConsole(self,dic) -> None:
-        """Функция записывает результат работы аналогичного метода на консоль"""
+    def repeat_of_every_word_to_console(self, dic) -> None:
+        """
+        Функция записывает результат работы аналогичного метода на консоль
+        """
+
         words = dic
 
         for word in words:
             print(word + ":" + str(words[word]))
         print('')
+
         return
 
-    def AverageNumberOfWordsInASentence(self,fileName) -> float:
-        """Функция поодсчитывает среднее количество слов в предложении
-        :param fileName: name of file
-        :type fileName: str
+    def average_number_of_words_in_a_sentence(self, file_name) -> float:
+        """
+        Функция поодсчитывает среднее количество слов в предложении
+        :param file_name: name of file
+        :type file_name: str
         :rtype: float
         :return: Среднее число слов в предложении
         """
 
-        countOfWords = 0
-        countOfSentences = 0
+        count_of_words = 0
+        count_of_sentences = 0
 
-        text = self.TextHelper(fileName)
+        text = self.text_helper(file_name)
 
         # получения количества слов.
         buff = text.split()
-        countOfWords = len(buff)
-        countOfSentences = len(text.split('.')) - 1
+        count_of_words = len(buff)
+        count_of_sentences = len(text.split('.')) - 1
 
-        if countOfSentences == 0:
+        if count_of_sentences == 0:
             return 0
         else:
-            return countOfWords / countOfSentences
+            return count_of_words / count_of_sentences
 
-    def AverageNumberOfWordsInASentenceToFile(self,fileName, ans) -> None:
-        """Функция заносит результат аналогичного метода в файл
-        :param fileName : name of file
-        :type fileName: str
+    def AverageNumberOfWordsInASentenceToFile(self, file_name, ans) -> None:
+        """
+        Функция заносит результат аналогичного метода в файл
+        :param file_name : name of file
+        :type file_name: str
         :param ans : average number of words in sentence
         :type ans: float
         """
-        file = open(fileName, "a")
+
+        file = open(file_name, "a")
 
         file.write("Среднее количество слов в предложении = " + str(ans))
         file.write('\n')
 
         file.close()
+
         return
 
-    def AverageNumberOfWordsInASentenceToConsole(self, ans) -> None:
-        """Функция заносит результат аналогичного метода на консоль
+    def average_number_of_words_in_a_sentence_to_console(self, ans) -> None:
+        """
+        Функция заносит результат аналогичного метода на консоль
         :param ans : average number of words in sentence
         :type ans: float
         """
 
         print("Среднее количество слов в предложении = " + str(ans)+"\n")
+
         return
 
-    def TextHelper(self,fileName) -> str:
-        """Функция чистит текст от лишних символов. ? ! ... Также раскрывает сокращения согласно правилам
-        :param fileName : name of file
-        :type fileName: str
+    def text_helper(self, file_name) -> str:
+        """
+        Функция чистит текст от лишних символов. ? ! ... Также раскрывает сокращения согласно правилам
+        :param file_name : name of file
+        :type file_name: str
         :rtype: str
         :return: Выходная строка - почищенный текст
         """
 
-        file = open(fileName, 'r')
+        file = open(file_name, 'r')
 
-        text = file.read()
-        text = text.lstrip()  # удаляю пробелы в начале
-        text = text.rstrip()  # удаляю пробелы в конце
+        text = file.read().lstrip().rstrip()
         text = text.replace('?', '.').replace('!', '.').replace('...', '.')
         text = text.replace('-', '', text.count('-'))
-
         text = text.replace('т.к.', 'так как', text.count('т.к.'))
         text = text.replace('т.п.', 'тому подобное', text.count('т.п.'))
         text = text.replace('т.д.', 'так далее', text.count('т.д.'))
         text = text.replace('др.', 'другое', text.count('др.'))
+
         file.close()
+
         return text
 
-    def MedianNumberOfWordsInSentence(self,fileName) -> int:
-        """Функция подсчитывает медианное количество слов в предложении
-        :param fileName: name of file
-        :type fileName:str
+    def median_number_of_words_in_sentence(self, file_name) -> int:
+        """
+        Функция подсчитывает медианное количество слов в предложении
+        :param file_name: name of file
+        :type file_name:str
         :rtype:int
         :return: Медианное число слов в предложении
         """
 
-        text = self.TextHelper(fileName)
+        text = self.text_helper(file_name)
+
         if text == '':
             return -1
 
-        listOfNumbersForEverySentence = []
+        list_of_numbers_for_every_sentence = []
 
-        listOfSentences = text.split(".")
-        if '' in listOfSentences:
-            listOfSentences.remove('')
+        list_of_sentences = text.split(".")
+        if '' in list_of_sentences:
+            list_of_sentences.remove('')
 
-        for sentense in listOfSentences:
+        for sentense in list_of_sentences:
             words = sentense.split()
-            listOfNumbersForEverySentence.append(len(words))
-        listOfNumbersForEverySentence.sort()
+            list_of_numbers_for_every_sentence.append(len(words))
+        list_of_numbers_for_every_sentence.sort()
 
-        medianAns = 0.
-        medianPos = (len(listOfNumbersForEverySentence) + 1) / 2
+        median_ans = 0.
+        medianPos = (len(list_of_numbers_for_every_sentence) + 1) / 2
         if medianPos is not int:
-            medianAns = (listOfNumbersForEverySentence[math.ceil(medianPos) - 1] + listOfNumbersForEverySentence[
+            median_ans = (list_of_numbers_for_every_sentence[math.ceil(medianPos) - 1] + list_of_numbers_for_every_sentence[
                 math.floor(medianPos) - 1]) / 2
         else:
-            medianAns = listOfNumbersForEverySentence[int(medianPos) - 1]
+            median_ans = list_of_numbers_for_every_sentence[int(medianPos) - 1]
 
-        # print(median(listOfNumbersForEverySentence))
+        return median_ans
 
-        return medianAns
-
-    def MedianNumberOfWordsInSentenceToFile(self,fileName, ans)-> None:
-        """Функция заносит результат аналогичного метода в файл
-        :param fileName: name of file
-        :type fileName: str
+    def median_number_of_words_in_sentence_to_file(self, file_name, ans)-> None:
+        """
+        Функция заносит результат аналогичного метода в файл
+        :param file_name: name of file
+        :type file_name: str
         :param ans: median number of words in sentence
         :type ans: int
         """
-        file = open(fileName, "a")
+        file = open(file_name, "a")
 
         file.write("Медианное количество слов в предложении = " + str(ans))
         file.write("\n\n")
 
         file.close()
+
         return
 
-    def MedianNumberOfWordsInSentenceToConsole(self, ans)-> None:
-        """Функция заносит результат аналогичного метода на консоль
+    def median_number_of_words_in_sentence_to_console(self, ans)-> None:
+        """
+        Функция заносит результат аналогичного метода на консоль
         :param ans: median number of words in sentence
         :type ans: int
         """
 
         print("Медианное количество слов в предложении = " + str(ans)+"\n")
+
         return
 
-    def HelpforTopfunc(self,fileName) -> str:
-        '''Функция подчищает текст в помощь основной чистящей функции. Она убирает парные знаки, которые для работы в поставленном ТЗ не нужны.
-        :param fileName: name of file
+    def helpfor_topfunc(self, file_name) -> str:
+        """Функция подчищает текст в помощь основной чистящей функции. Она убирает парные знаки, которые для работы в поставленном ТЗ не нужны.
+        :param file_name: name of file
         :type: str
-        :rtype:str'''
-        text = self.TextHelper(fileName)
+        :rtype:str
+        """
+
+        text = self.text_helper(file_name)
         text = text.lower()
-        for item in self.stopSymbols2:
+
+        for item in stop_symbols2:
             text = text.replace(item, '', text.count(item))
 
         text = text.replace('...', '', text.count('...'))
@@ -224,82 +234,94 @@ class WorkWithText:
         text = text.replace('"', '', text.count('"'))
         text = text.replace("'", '', text.count("'"))
         text.replace("/", '', text.count('/'))
+
         return text
 
-    def GetKey(self,dictionary, value)->str:
-        '''Функция реализована для поиска ключа по значению в словаре.
+    def get_key(self, dictionary, value)->str:
+        """
+        Функция реализована для поиска ключа по значению в словаре.
         :param dictionary: dictionary with keys to find
         :type dictionary: dict
         :rtype: str
-        :return: Возращает ключ(строку)'''
+        :return: Возращает ключ(строку)
+        """
+
         for key, val in dictionary.items():
             if val == value:
                 return key
 
-    def TopK(self,fileName, N=4, K=10):
-        """Функция возвращает словарь самых популярных N-грамм в тексте
-        :param fileName: name of file
-        :type fileName: str
+    def top_k(self, file_name, N=4, K=10):
+        """
+        Функция возвращает словарь самых популярных N-грамм в тексте
+        :param file_name: name of file
+        :type file_name: str
         :param N: number of symbols in N-gramm
         :type N; int
         :param K: number of Top-K
         :type K: int
         :rtype:dict
-        :return: Словарь самых популярных N-грамм"""
+        :return: Словарь самых популярных N-грамм
+        """
 
-        text = self.HelpforTopfunc(fileName)
+        text = self.helpfor_topfunc(file_name)
         # text = text.replace(item,'',text.count(item)) for item in stopSymbols2  как это провернуть?
         ans = dict()
 
-        listOfSentences = [sentence.split() for sentence in text.split(".")]
-        for listOfWords in listOfSentences:
-            for word in listOfWords:
+        list_of_sentences = [sentence.split() for sentence in text.split(".")]
+
+        for list_of_words in list_of_sentences:
+            for word in list_of_words:
                 if len(word) >= N:
                     for i in range(0, len(word) - N + 1):
                         if word[i: i + N] in ans:
                             ans[word[i: i + N]] += 1
                         else:
                             ans[word[i: i + N]] = 1
-                else:
-                    continue
 
         return ans
 
-    def TopKToFile(self,ans, fileName, K=10)-> None:
-        """Функция заносит результат аналогичного метода в файл
+    def top_k_to_file(self, ans, file_name, K=10)-> None:
+        """
+        Функция заносит результат аналогичного метода в файл
         :param ans : dictionary with N-gramms
         :type ans: dict
-        :param fileName: name of file
-        :type fileName: str
+        :param file_name: name of file
+        :type file_name: str
         :param K: number of Top-K
-        :type K: int"""
+        :type K: int
+        """
 
-        file = open(fileName, "a")
+        file = open(file_name, "a")
 
-        sortedValues = sorted(ans.values())
-        sortedValues.reverse()
+        sorted_values = sorted(ans.values())
+        sorted_values.reverse()
 
         file.write(f"Топ {K}:" + "\n")
         for i in range(0, K):
-            file.write(self.GetKey(ans,sortedValues[i]) + '-' + str(sortedValues[i]) + "\n")
-            ans.pop(self.GetKey(ans, sortedValues[i]))
+            file.write(self.get_key(ans, sorted_values[i]) + '-' + str(sorted_values[i]) + "\n")
+            ans.pop(self.get_key(ans, sorted_values[i]))
 
         file.close()
+
         return
 
-    def TopKToConsole(self,ans, K=10)-> None:
-        """Функция заносит результат аналогичного метода на консоль
+    def top_k_to_console(self, ans, K=10)-> None:
+        """
+        Функция заносит результат аналогичного метода на консоль
         :param ans : dictionary with N-gramms
         :type ans: dict
         :param K: number of Top-K
-        :type K: int"""
+        :type K: int
+        """
 
-        sortedValues = sorted(ans.values())
-        sortedValues.reverse()
+        sorted_values = sorted(ans.values())
+        sorted_values.reverse()
 
         print(f"Топ {K}:")
+
         for i in range(0, K):
-            print(self.GetKey(ans,sortedValues[i]) + '-' + str(sortedValues[i]))
-            ans.pop(self.GetKey(ans, sortedValues[i]))
+            print(self.get_key(ans, sorted_values[i]) + '-' + str(sorted_values[i]))
+            ans.pop(self.get_key(ans, sorted_values[i]))
         print("\n")
+
         return
--- solution.py
+++ solution.py
@@ -1,4 +1,6 @@
 from WorkingClass.WorkWithText import WorkWithText
+from WorkingClass.constants import stop_symbols
+from WorkingClass.constants import stop_symbols2
 
 def main():
     tool = WorkWithText()
@@ -7,10 +9,11 @@ def main():
     if path == '':
         path = "WorkingClass/text.txt"
 
-    tool.RepeatOfEveryWordToConsole(tool.RepeatOfEveryWord(path))  # количество всех слов в тексте
-    tool.AverageNumberOfWordsInASentenceToConsole(tool.AverageNumberOfWordsInASentence(path))
-    tool.MedianNumberOfWordsInSentenceToConsole(tool.MedianNumberOfWordsInSentence(path))
-    tool.TopKToConsole(tool.TopK(path, 3, 6), 6)
+    tool.repeat_of_every_word_to_console(tool.repeat_of_every_word(path))  # количество всех слов в тексте
+    tool.average_number_of_words_in_a_sentence_to_console(tool.average_number_of_words_in_a_sentence(path))
+    tool.median_number_of_words_in_sentence_to_console(tool.median_number_of_words_in_sentence(path))
+    tool.top_k_to_console(tool.top_k(path, 3, 6), 6)
+
 
 if __name__ == "__main__":
    main()
