466df3820a2bc20bba1ca30aae0c81bd0debe07d
Aleh <igrock.iks@mail.ru>
2022-03-09 23:50:06 +0300

    маленькие правки

--- WorkingClass/WorkWithText.py
+++ WorkingClass/WorkWithText.py
@@ -2,13 +2,14 @@ import math
 from WorkingClass.constants import stop_symbols
 from WorkingClass.constants import stop_symbols2
 
+
 class WorkWithText:
     """Класс для работы с входным текстом"""
 
     def __init__(self) -> None:
         return
 
-    def repeat_of_every_word(self, file_name) -> dict:
+    def repeat_of_every_word(self, file_name: str) -> dict:
         """
         Метод нахождения количества повторений каждого слова в тексте.
         :rtype: dict
@@ -45,7 +46,8 @@ class WorkWithText:
 
         return response
 
-    def repeat_of_every_word_to_file(self, file_name, dic) -> None:
+    @staticmethod
+    def repeat_of_every_word_to_file(file_name: str, dic: dict) -> None:
         """
         Функция записывает результат работы аналогичного метода в файл
         """
@@ -62,7 +64,8 @@ class WorkWithText:
 
         return
 
-    def repeat_of_every_word_to_console(self, dic) -> None:
+    @staticmethod
+    def repeat_of_every_word_to_console(dic: dict) -> None:
         """
         Функция записывает результат работы аналогичного метода на консоль
         """
@@ -75,7 +78,7 @@ class WorkWithText:
 
         return
 
-    def average_number_of_words_in_a_sentence(self, file_name) -> float:
+    def average_number_of_words_in_a_sentence(self, file_name: str) -> float:
         """
         Функция поодсчитывает среднее количество слов в предложении
         :param file_name: name of file
@@ -99,7 +102,8 @@ class WorkWithText:
         else:
             return count_of_words / count_of_sentences
 
-    def AverageNumberOfWordsInASentenceToFile(self, file_name, ans) -> None:
+    @staticmethod
+    def average_number_of_words_in_a_sentence_to_file(file_name: str, ans: float) -> None:
         """
         Функция заносит результат аналогичного метода в файл
         :param file_name : name of file
@@ -117,7 +121,8 @@ class WorkWithText:
 
         return
 
-    def average_number_of_words_in_a_sentence_to_console(self, ans) -> None:
+    @staticmethod
+    def average_number_of_words_in_a_sentence_to_console(ans: float) -> None:
         """
         Функция заносит результат аналогичного метода на консоль
         :param ans : average number of words in sentence
@@ -128,7 +133,8 @@ class WorkWithText:
 
         return
 
-    def text_helper(self, file_name) -> str:
+    @staticmethod
+    def text_helper(file_name: str) -> str:
         """
         Функция чистит текст от лишних символов. ? ! ... Также раскрывает сокращения согласно правилам
         :param file_name : name of file
@@ -151,7 +157,7 @@ class WorkWithText:
 
         return text
 
-    def median_number_of_words_in_sentence(self, file_name) -> int:
+    def median_number_of_words_in_sentence(self, file_name: str) -> int:
         """
         Функция подсчитывает медианное количество слов в предложении
         :param file_name: name of file
@@ -168,25 +174,28 @@ class WorkWithText:
         list_of_numbers_for_every_sentence = []
 
         list_of_sentences = text.split(".")
+
         if '' in list_of_sentences:
             list_of_sentences.remove('')
 
-        for sentense in list_of_sentences:
-            words = sentense.split()
+        for sentence in list_of_sentences:
+            words = sentence.split()
             list_of_numbers_for_every_sentence.append(len(words))
         list_of_numbers_for_every_sentence.sort()
 
         median_ans = 0.
-        medianPos = (len(list_of_numbers_for_every_sentence) + 1) / 2
-        if medianPos is not int:
-            median_ans = (list_of_numbers_for_every_sentence[math.ceil(medianPos) - 1] + list_of_numbers_for_every_sentence[
-                math.floor(medianPos) - 1]) / 2
+        median_pos = (len(list_of_numbers_for_every_sentence) + 1) / 2
+        if median_pos is not int:
+            median_ans = (list_of_numbers_for_every_sentence[math.ceil(median_pos) - 1]
+                          + list_of_numbers_for_every_sentence[
+                math.floor(median_pos) - 1]) / 2
         else:
-            median_ans = list_of_numbers_for_every_sentence[int(medianPos) - 1]
+            median_ans = list_of_numbers_for_every_sentence[int(median_pos) - 1]
 
         return median_ans
 
-    def median_number_of_words_in_sentence_to_file(self, file_name, ans)-> None:
+    @staticmethod
+    def median_number_of_words_in_sentence_to_file(file_name: str, ans: int) -> None:
         """
         Функция заносит результат аналогичного метода в файл
         :param file_name: name of file
@@ -203,7 +212,8 @@ class WorkWithText:
 
         return
 
-    def median_number_of_words_in_sentence_to_console(self, ans)-> None:
+    @staticmethod
+    def median_number_of_words_in_sentence_to_console(ans: int) -> None:
         """
         Функция заносит результат аналогичного метода на консоль
         :param ans: median number of words in sentence
@@ -214,8 +224,9 @@ class WorkWithText:
 
         return
 
-    def helpfor_topfunc(self, file_name) -> str:
-        """Функция подчищает текст в помощь основной чистящей функции. Она убирает парные знаки, которые для работы в поставленном ТЗ не нужны.
+    def helpfor_topfunc(self, file_name: str) -> str:
+        """Функция подчищает текст в помощь основной чистящей функции. Она убирает парные знаки,
+         которые для работы в поставленном ТЗ не нужны.
         :param file_name: name of file
         :type: str
         :rtype:str
@@ -237,28 +248,29 @@ class WorkWithText:
 
         return text
 
-    def get_key(self, dictionary, value)->str:
+    @staticmethod
+    def get_key(dictionary: dict, value: int) -> str:
         """
         Функция реализована для поиска ключа по значению в словаре.
-        :param dictionary: dictionary with keys to find
+        :param dictionary: словарь, в котором ищем
         :type dictionary: dict
+        :param value: значение, по которому вернётся ключ
+        :type: int
         :rtype: str
-        :return: Возращает ключ(строку)
+        :return: Возвращает ключ(строку)
         """
 
         for key, val in dictionary.items():
             if val == value:
                 return key
 
-    def top_k(self, file_name, N=4, K=10):
+    def top_k(self, file_name: str, n=4) -> dict:
         """
         Функция возвращает словарь самых популярных N-грамм в тексте
         :param file_name: name of file
         :type file_name: str
-        :param N: number of symbols in N-gramm
-        :type N; int
-        :param K: number of Top-K
-        :type K: int
+        :param n: number of symbols in N-gramm
+        :type n; int
         :rtype:dict
         :return: Словарь самых популярных N-грамм
         """
@@ -271,24 +283,25 @@ class WorkWithText:
 
         for list_of_words in list_of_sentences:
             for word in list_of_words:
-                if len(word) >= N:
-                    for i in range(0, len(word) - N + 1):
-                        if word[i: i + N] in ans:
-                            ans[word[i: i + N]] += 1
+                if len(word) >= n:
+                    for i in range(0, len(word) - n + 1):
+                        if word[i: i + n] in ans:
+                            ans[word[i: i + n]] += 1
                         else:
-                            ans[word[i: i + N]] = 1
+                            ans[word[i: i + n]] = 1
 
         return ans
 
-    def top_k_to_file(self, ans, file_name, K=10)-> None:
+    @staticmethod
+    def top_k_to_file(ans, file_name, k=10) -> None:
         """
         Функция заносит результат аналогичного метода в файл
         :param ans : dictionary with N-gramms
         :type ans: dict
         :param file_name: name of file
         :type file_name: str
-        :param K: number of Top-K
-        :type K: int
+        :param k: number of Top-K
+        :type k: int
         """
 
         file = open(file_name, "a")
@@ -296,32 +309,33 @@ class WorkWithText:
         sorted_values = sorted(ans.values())
         sorted_values.reverse()
 
-        file.write(f"Топ {K}:" + "\n")
-        for i in range(0, K):
-            file.write(self.get_key(ans, sorted_values[i]) + '-' + str(sorted_values[i]) + "\n")
-            ans.pop(self.get_key(ans, sorted_values[i]))
+        file.write(f"Топ {k}:" + "\n")
+        for i in range(0, k):
+            file.write(WorkWithText.get_key(ans, sorted_values[i]) + '-' + str(sorted_values[i]) + "\n")
+            ans.pop(WorkWithText.get_key(ans, sorted_values[i]))
 
         file.close()
 
         return
 
-    def top_k_to_console(self, ans, K=10)-> None:
+    @staticmethod
+    def top_k_to_console(ans: dict, k=10) -> None:
         """
         Функция заносит результат аналогичного метода на консоль
         :param ans : dictionary with N-gramms
         :type ans: dict
-        :param K: number of Top-K
-        :type K: int
+        :param k: number of Top-K
+        :type k: int
         """
 
         sorted_values = sorted(ans.values())
         sorted_values.reverse()
 
-        print(f"Топ {K}:")
+        print(f"Топ {k}:")
 
-        for i in range(0, K):
-            print(self.get_key(ans, sorted_values[i]) + '-' + str(sorted_values[i]))
-            ans.pop(self.get_key(ans, sorted_values[i]))
+        for i in range(0, k):
+            print(WorkWithText.get_key(ans, sorted_values[i]) + '-' + str(sorted_values[i]))
+            ans.pop(WorkWithText.get_key(ans, sorted_values[i]))
         print("\n")
 
         return
--- solution.py
+++ solution.py
@@ -2,6 +2,7 @@ from WorkingClass.WorkWithText import WorkWithText
 from WorkingClass.constants import stop_symbols
 from WorkingClass.constants import stop_symbols2
 
+
 def main():
     tool = WorkWithText()
     print("Введите путь к файлу")
@@ -9,11 +10,11 @@ def main():
     if path == '':
         path = "WorkingClass/text.txt"
 
-    tool.repeat_of_every_word_to_console(tool.repeat_of_every_word(path))  # количество всех слов в тексте
-    tool.average_number_of_words_in_a_sentence_to_console(tool.average_number_of_words_in_a_sentence(path))
-    tool.median_number_of_words_in_sentence_to_console(tool.median_number_of_words_in_sentence(path))
-    tool.top_k_to_console(tool.top_k(path, 3, 6), 6)
+    WorkWithText.repeat_of_every_word_to_console(tool.repeat_of_every_word(path))  # количество всех слов в тексте
+    WorkWithText.average_number_of_words_in_a_sentence_to_console(tool.average_number_of_words_in_a_sentence(path))
+    WorkWithText.median_number_of_words_in_sentence_to_console(tool.median_number_of_words_in_sentence(path))
+    WorkWithText.top_k_to_console(tool.top_k(path), 6)
 
 
-if __name__ == "__main__":
-   main()
+if __name__ == '__main__':
+    main()
