c07100ad91e50367eb1030079f90cde57bf38669
Aleh <trapper.md@mail.ru>
2022-04-14 18:30:23 +0300

    Теперь добавил фабрику и атрибуты для функций и их кода

---
+++ Lab-2/lib/Serializers/Factory/SerializerFactory.py
@@ -0,0 +1,24 @@
+from lib.Serializers.JSONSerializer import JSONSerializer
+from lib.Serializers.YAMLSerializer import YAMLSerializer
+from lib.Serializers.TOMLSerializer import TOMLSerializer
+
+
+def factory(type_of_serializer: str):
+    """
+    Метод создания нужного нам сериализатора в зависимости от запроса
+
+    :param type_of_serializer: тип нужного сериализатора
+    :type type_of_serializer: str
+
+    :return: Возвращает сериализатор нужного нам формата
+    :rtype: Serializer
+    """
+
+    type_of_serializer = type_of_serializer.lower()
+
+    if type_of_serializer == 'json':
+        return JSONSerializer()
+    elif type_of_serializer == 'yaml':
+        return YAMLSerializer()
+    elif type_of_serializer == 'toml':
+        return TOMLSerializer()
