40a6f32483ad4b808f78e8456a1744dde04baf9a
Aleh <trapper.md@mail.ru>
2022-05-16 23:23:24 +0300

    Изменил концепцию приложения под кандидатов на одни выборы, а не выборы и кандидатов. Добавил модель кандидатов. Партии(отношение 1 ко многим). Слоганы кандидатов(отношение 1 к 1). Сделал миграции бд. Слегка настроил админку на поиск, редактирование, сортировку и тд.

--- Lab-3/Elections/GOLOSOVANIE/admin.py
+++ Lab-3/Elections/GOLOSOVANIE/admin.py
@@ -1,3 +1,31 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import StartPage, Candidate, Slogan, Batch
+
+
+class StartPageAdmin(admin.ModelAdmin):
+    list_display = ('img',)
+
+
+class CandidateAdmin(admin.ModelAdmin):
+    list_display = ('id', 'first_name', 'last_name', 'batch', 'date_of_birth', 'region',)
+    search_fields = ('region', 'last_name')
+    list_display_links = ('id', 'first_name', 'last_name',)
+    list_filter = ('date_of_birth', 'region', 'batch')
+    list_editable = ('batch', 'region')
+
+
+class SloganAdmin(admin.ModelAdmin):
+    list_display = ('id', 'slogan')
+
+
+class BatchAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'political_views')
+    search_fields = ('political_views',)
+    list_display_links = ('id', 'name',)
+
+
+admin.site.register(StartPage, StartPageAdmin)
+admin.site.register(Candidate, CandidateAdmin)
+admin.site.register(Slogan, SloganAdmin)
+admin.site.register(Batch, BatchAdmin)
--- Lab-3/Elections/GOLOSOVANIE/models.py
+++ Lab-3/Elections/GOLOSOVANIE/models.py
@@ -2,4 +2,56 @@ from django.db import models
 
 
 class StartPage(models.Model):
-    arztozka_img = models.ImageField(upload_to='photos/Start_Page/')
+    img = models.ImageField(upload_to='photos/Start_Page/')
+
+    class Meta:
+        verbose_name = 'Стартовая страница'
+        verbose_name_plural = 'Стартовая страницы'
+
+
+def path_to_directory(instance, filename):
+    return 'photos/Elections/{0}/{1}/{2}'.format(instance.last_name, instance.first_name, filename)
+
+
+class Candidate(models.Model):
+    first_name = models.CharField(max_length=150, verbose_name='Имя')
+    last_name = models.CharField(max_length=150, verbose_name='Фамилия')
+    middle_name = models.CharField(max_length=150, verbose_name='Отчество')
+    date_of_birth = models.DateField(verbose_name='Дата рождения')
+    region = models.CharField(max_length=150, verbose_name='Регион')
+    description = models.TextField()
+    preview = models.ImageField(upload_to=path_to_directory)
+    #slogan = models.ForeignKey('Slogan', on_delete=models.PROTECT, verbose_name='Слоган', null=True)
+    batch = models.ForeignKey('Batch', on_delete=models.CASCADE, verbose_name='Партия', null=True, blank=True) # многие к одному, в параметры передаётся именно то, чего будет 1 штука
+    slogan = models.OneToOneField('Slogan', on_delete=models.PROTECT, verbose_name='Слоган', null=True)
+
+    class Meta:
+        verbose_name = 'Кандидат'
+        verbose_name_plural = 'Кандидаты'
+        ordering = ('-date_of_birth',)
+
+    def __str__(self):
+        return self.first_name + self.last_name
+
+
+class Slogan(models.Model):
+    slogan = models.CharField(max_length=150)
+
+    class Meta:
+        verbose_name = 'Слоган'
+        verbose_name_plural = 'Слоганы'
+
+    def __str__(self):
+        return self.slogan
+
+
+class Batch(models.Model):
+    name = models.CharField(max_length=150)
+    political_views = models.CharField(max_length=150)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Партия'
+        verbose_name_plural = 'Партии'
